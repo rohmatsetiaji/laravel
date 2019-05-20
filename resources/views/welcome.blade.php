891c193033fddac8617bfbab31a57a0b4720dc99
rohmat setiaji <arohmat465@gmail.com>
2019-05-20 21:30:58 +0700

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Rohmat
                 </div>
 
                 <div class="links">
